91906a75f37d1989da1db6ebba183fa5ab79ff9a
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2024-02-26 23:31:37 +0900

    add_email_custom_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+  before_validation :downcase_email
+
   # gem 'bcrypt'
   has_secure_password
   # validates
@@ -8,6 +12,9 @@ class User < ApplicationRecord
                      allow_blank: true, # Null, 空白文字の場合スキップ
                    }
 
+  validates :email, presence: true,
+                    email: { allow_blank: true }
+
   validates :password, presence: true, # 入力必須
                        length: { minimum: 8, allow_blank: true }, # 最小文字数
                        format: {
@@ -16,4 +23,24 @@ class User < ApplicationRecord
                          allow_blank: true,
                        },
                        allow_nil: true # 空パスワードのアップデートを許容(ユーザ新規登録時はhas_secure_passwordが検証する)
+  ## methods
+  class << self
+    # emailからアクティブなユーザーを返す
+    def find_by_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+
+  # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_by_activated(email).present?
+  end
+
+  private
+
+    # email小文字化
+    def downcase_email
+      self.email.downcase! if email
+    end
 end
