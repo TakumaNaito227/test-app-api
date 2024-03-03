ad5c6afc478d949d69d72359b56eb873f4732751
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2024-03-04 00:35:18 +0900

    users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+    users = User.all
+    # as_json: ハッシュの形でjsonを返す
+    render json: users.as_json(only: [:id, :name, :email, :created_ad])
+  end
+end
