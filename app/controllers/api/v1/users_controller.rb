c7ca081019e534b28415fb678541cb61919c376e
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2024-03-04 23:25:05 +0900

    fix users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::UsersController < ApplicationController
   def index
     users = User.all
     # as_json: ハッシュの形でjsonを返す
-    render json: users.as_json(only: [:id, :name, :email, :created_ad])
+    render json: users.as_json(only: [:id, :name, :email, :created_at])
   end
 end
